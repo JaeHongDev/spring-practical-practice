4f512d9e263ad96c216981d6cd88676fdbae7118
JaehongDev <println@kakao.com>
2022-09-02 14:10:28 +0900

    chapter1

--- chapter1/src/Flux.java
+++
@@ -1,5 +0,0 @@
-public class Flux<T> {
-    public static Flux<Dish> just(Dish sesame_chicken, Dish dish, Dish dish1) {
-        return new Flux<Dish>();
-    }
-}
--- chapter1/src/KitchenService.java
+++
@@ -1,9 +0,0 @@
-public class KitchenService {
-    Flux<Dish> getDishes(){
-        return Flux.just(
-                new Dish("Sesame chicken"),
-                new Dish("Lo mein noodles, plain"),
-                new Dish("Sweet & sour beef"));
-    }
-
-}
